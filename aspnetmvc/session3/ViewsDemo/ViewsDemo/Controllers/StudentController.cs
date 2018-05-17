0476f076af0ae953d07e1f46f90cefc839f337e7
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-05-17 20:54:54 +0200

    added various action results

--- aspnetmvc/session3/ViewsDemo/ViewsDemo/Controllers/StudentController.cs
+++ aspnetmvc/session3/ViewsDemo/ViewsDemo/Controllers/StudentController.cs
@@ -8,17 +8,56 @@ namespace ViewsDemo.Controllers
 {
     public class StudentController : Controller
     {
-        // GET: Student
+        //StatusCodes
+        //100
+        //200 success
+        //203 success NoContent
+        //400 BadRequest /students?start=-10&limit=
+        //401 Unauthenticated -> Unauthorized
+        //403 UnAuthorized -> Forbidden
+        //500 forgotten try-catch
+
         public ActionResult Info()
         {
             return View();
         }
 
-
-        // GET: Student
         public ActionResult Index()
         {
             return View();
         }
+
+        [ActionName("view")]
+        //[Route("design/view/{id}")]
+        public ViewResult ViewResult(int id)
+        {
+            return View("~/Views/Student/View.cshtml");
+        }
+
+        [ActionName("partialview")]
+        public PartialViewResult PartialViewResult()
+        {
+            return PartialView();
+        }
+
+        [ActionName("json")]
+        public JsonResult JsonResult()
+        {
+            var anonymous = new
+            {
+                age = 2,
+                firstName = "dzevo",
+                lastName = "ibraimi"
+            };
+
+            return Json(anonymous, JsonRequestBehavior.AllowGet);
+        }
+
+        [ActionName("toindex")]
+        public ActionResult RedirectToIndex()
+        {
+            return RedirectToAction("Index");
+        }
+
     }
 }
