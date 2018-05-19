49fb9e5fca5da612620e0cb59936617cf5e24121
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-05-19 11:28:53 +0200

    added custom attributes

--- aspnetmvc/session3/ViewsDemo/ViewsDemo/Controllers/PictureController.cs
+++ aspnetmvc/session3/ViewsDemo/ViewsDemo/Controllers/PictureController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,12 +11,25 @@ namespace ViewsDemo.Controllers
     public class PictureController : Controller
     {
         public ActionResult Index()
-        { 
+        {
             /*backed code here (database calls, api calls ....)*/
             return View(new List<Picture>() {
                 new Picture{Id=1,DisplayName="asd",Url="http://asd"},
                 new Picture{Id=2,DisplayName="qwe",Url="http://qwe"},
             });
+        }
+
+        public ActionResult Create()
+        {
+            //string path =  Directory.GetCurrentDirectory();
+            return View("~/Views/Picture/CreateWithHelpers.cshtml");
+        }
+
+        [HttpPost]
+        public ActionResult Create(Picture picture)
+        {
+            
+            return View();
         }
     }
 }
--- aspnetmvc/session3/ViewsDemo/ViewsDemo/Models/Picture.cs
+++ aspnetmvc/session3/ViewsDemo/ViewsDemo/Models/Picture.cs
@@ -1,9 +1,16 @@
-﻿namespace ViewsDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewsDemo.Models
 {
     public class Picture
     {
         public int Id { get; set; }
+        
+        //[Custom("some error message","optional param",MinValue =1,MaxValue =10)]
+        [Required(AllowEmptyStrings =false, ErrorMessage = "DisplayName is required")]
         public string DisplayName { get; set; }
+
+
         public string Url { get; set; }
     }
 }
