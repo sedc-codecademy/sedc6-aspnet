a29c15815907c88e2ffcdfee576b447aae4e9377
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-05-19 10:18:57 +0200

    added picture controller

---
+++ aspnetmvc/session3/ViewsDemo/ViewsDemo/Controllers/PictureController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewsDemo.Models;
+
+namespace ViewsDemo.Controllers
+{
+    public class PictureController : Controller
+    {
+        public ActionResult Index()
+        { 
+            /*backed code here (database calls, api calls ....)*/
+            return View(new List<Picture>() {
+                new Picture{Id=1,DisplayName="asd",Url="http://asd"},
+                new Picture{Id=2,DisplayName="qwe",Url="http://qwe"},
+            });
+        }
+    }
+}
---
+++ aspnetmvc/session3/ViewsDemo/ViewsDemo/Models/Picture.cs
@@ -0,0 +1,9 @@
+﻿namespace ViewsDemo.Models
+{
+    public class Picture
+    {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+        public string Url { get; set; }
+    }
+}
