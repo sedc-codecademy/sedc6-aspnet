b72921a7bd670dd2387b9e55e97cea1b8739c2cb
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-05-22 18:28:26 +0200

    added models for music manager

---
+++ aspnetmvc/session5/Sedc.MusicManager/Sedc.MusicManager.WebApp/Models/Song.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Sedc.MusicManager.WebApp.Models
+{
+    public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int Duration { get; set; }
+
+        [ForeignKey("Album")]
+        public int AlbumId { get; set; }
+
+        public virtual Album Album { get; set; }
+    }
+}
