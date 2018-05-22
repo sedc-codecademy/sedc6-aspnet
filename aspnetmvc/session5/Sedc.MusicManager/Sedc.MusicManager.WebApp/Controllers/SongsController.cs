b898129b5ab427680970988fc3a72be122cb40ea
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-05-22 19:34:54 +0200

    added index view

---
+++ aspnetmvc/session5/Sedc.MusicManager/Sedc.MusicManager.WebApp/Controllers/SongsController.cs
@@ -0,0 +1,28 @@
+﻿using Sedc.MusicManager.WebApp.Data;
+using Sedc.MusicManager.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sedc.MusicManager.WebApp.Controllers
+{
+    public class SongsController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            MusicDb db = new MusicDb();
+            
+            IQueryable<Song> results = db.Songs
+                .Where(s => s.Duration > 100)
+                .Where(s => s.Duration > 100)
+                .Where(s => s.Duration > 100)   ;
+
+            List<Song> songs = results.ToList();
+
+            return View(songs);
+        }
+    }
+}
