0a9c1313f553aa8aa9c01ce8333b627063f91611
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-06-02 09:53:35 +0200

    added models

---
+++ aspnetmvc/session8/Sedc.MusicManagement.Models/ArtistType.cs
@@ -0,0 +1,8 @@
+﻿namespace Sedc.MusicManager.DomainModels
+{
+    public enum ArtistType
+    {
+        Band = 1,
+        SoloArtist = 2
+    }
+}
---
+++ aspnetmvc/session8/Sedc.MusicManagement.Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sedc.MusicManager.DomainModels
+{
+    public enum Genre
+    {
+        Pop = 1,
+        HipHop =2,
+        RnB = 3
+    }
+}
