15cdf1e797627ff382ca1fa9d53d726a5c71f0d7
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-06-02 11:47:47 +0200

    added entity framework config

--- aspnetmvc/session8/Sedc.MusicManagement.Repositories.EntityFramework/MusicDb.cs
+++ aspnetmvc/session8/Sedc.MusicManagement.Repositories.EntityFramework/MusicDb.cs
@@ -1,10 +1,15 @@
 ﻿using Sedc.MusicManagement.Models;
+using System.Configuration;
 using System.Data.Entity;
 
 namespace Sedc.MusicManagement.Repositories.EntityFramework
 {
     public class MusicDb : DbContext
     {
+        public MusicDb()
+        {
+
+        }
         public DbSet<Artist> Artists { get; set; }
     }
 }
